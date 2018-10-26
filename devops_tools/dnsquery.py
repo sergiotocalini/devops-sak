bd15506c99e09c3b4745115d787a709a6ab05f76
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-10-26 14:16:45 +0200

    Removing all code

--- devops_tools/dnsquery.py
+++ devops_tools/dnsquery.py
@@ -19,17 +19,10 @@ class DNSQuery():
         return zone, records
 
     def get_records(self, opt):
-        opt.setdefault('environment', None)
         opt.setdefault('grep', '')
         opt.setdefault('regex', '.*')
-        opt.setdefault('subdomain', '')
         opt.setdefault('type', ['A', 'CNAME'])
 
-        if opt['environment'] != None:
-            opt['domain'] = '%(environment)s.%(domain)s' %opt
-
-        filter_subdomain = re.compile('.*%s$' %opt['subdomain'],
-                                      re.IGNORECASE)
         filter_hostname = re.compile(opt['regex'], re.IGNORECASE)
 
         zone, records = self.get_zone(opt['domain'])
@@ -37,17 +30,16 @@ class DNSQuery():
         for i in records:
             for x in zone[i].to_text(i).split("\n"):
                 row = x.split(" ")
-                if filter_subdomain.match(row[0]):
-                    if (opt['type'] == "*") or (row[3] in opt['type']):
-                        if filter_hostname.match(row[0]):
-                            if (opt['grep'].lower() in row[0].lower()):
-                                hostname = '.'.join([row[0], opt['domain']])
-                                list_rec.append(
-                                    {
-                                        'ip':row[4], 'host':hostname,
-                                        'type':row[3] , 'ttl':row[1]
-                                    }
-                                )
+                if (opt['type'] == "*") or (row[3] in opt['type']):
+                    if filter_hostname.match(row[0]):
+                        if (opt['grep'].lower() in row[0].lower()):
+                            hostname = '.'.join([row[0], opt['domain']])
+                            list_rec.append(
+                                {
+                                    'ip':row[4], 'host':hostname,
+                                    'type':row[3] , 'ttl':row[1]
+                                }
+                            )
         return list_rec
                 
     def display_output(self, records, options, delimiter):
@@ -59,33 +51,30 @@ class DNSQuery():
 def main():
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("-G", "--grep", dest="grep", default='',
-                      help="Grep output.", metavar="str")
     parser.add_option("-d", "--domain", dest="domain",
                       default="aws.vostu.com",
                       help="Domain", metavar="str")
-    parser.add_option("-e", "--environment", dest="env", default=None,
-                      help="Environment query.", metavar="str")
     parser.add_option("-f", "--fields", dest="fields", default="ip,host",
                       help="Display fields (ip, name, type).",
                       metavar="list")
-    parser.add_option("-g", "--game", dest="sub", default='',
-                      help="Game query.", metavar="str")
     parser.add_option("-r", "--regex", dest='regex', default='.*',
                       help="Regular Expression.", metavar="str")
     parser.add_option("-s", "--server", dest="server", default="dns1",
                       help="DNS Server", metavar="str")
     parser.add_option("-t", "--type", dest="rtype", default="A,CNAME",
                       help="Type filter", metavar="list")
-    parser.add_option("--delimiter", dest="delimiter", default="\t",
+    parser.add_option("-D", "--delimiter", dest="delimiter", default="\t",
                       help="Delimiter", metavar="str")
     (options, args) = parser.parse_args()
     if options.server and options.domain:
-        options = {'domain': options.domain, 'subdomain': options.sub,
-                   'type': options.rtype.split(","), 'grep':options.grep,
-                   'regex': options.regex, 'environment': options.env,
-                   'server': options.server, 'delimiter': options.delimiter,
-                   'fields': options.fields.split(",")}
+        options = {
+            'domain': options.domain,
+            'type': [t.upper() for t in options.rtype.split(",")],
+            'regex': options.regex,
+            'server': options.server,
+            'delimiter': options.delimiter,
+            'fields': options.fields.split(",")
+        }
         DNSAdmin = DNSQuery(options['server'])
         Records = DNSAdmin.get_records(options)
         DNSAdmin.display_output(Records, options['fields'], 
